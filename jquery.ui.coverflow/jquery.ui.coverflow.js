6fd3315fe382d02f50218218d3fc107b8f8f5de8
Ben New <ben@leftclick.com.au>
2011-04-29 07:47:14 +0000

    Add "wrap-around" effect, where items are moved from the start to the end and vice versa, when the position is changed.

--- jquery.ui.coverflow/jquery.ui.coverflow.js
+++ jquery.ui.coverflow/jquery.ui.coverflow.js
@@ -24,25 +24,27 @@
 		options: {
 			items: '> *',
 			orientation: 'horizontal',
-			item: 0,
+			item: 'middle',
 			trigger: 'click',
+			duration: 'normal',
+			currentItemZoom: 0.3,
 			center: true, // If false, element's base position isn't touched in any way
 			recenter: true, // If false, the parent element's position doesn't get animated while items change
 			autoAdvance: false, // If a number, the nex() function is called automatically once every [value] ms
-			pauseOnHover: true // If false, don't pause the auto-advance on mouseover; ignored if autoAdvance is false
+			pauseOnHover: true, // If false, don't pause the auto-advance on mouseover; ignored if autoAdvance is false
+			refreshOnWindowResize: true // If false, don't automatically refresh when the window is resized
 		},
 		
 		_create: function() {
 			var self = this, o = this.options;
 			this.items = $(o.items, this.element);
 			this.props = o.orientation == 'vertical' ? ['height', 'Height', 'top', 'Top'] : ['width', 'Width', 'left', 'Left'];
-			// For < 1.8.2: this.items['outer'+this.props[1]](1);
-
+			this.transformProp = ($.browser.safari ? 'webkit' : 'Moz')+'Transform';
 			this.itemSize = 0.4 * this.items.innerWidth();
 			this.itemWidth = this.items.width();
 			this.itemHeight = this.items.height();
-			this.duration = o.duration;
 			this.current = (o.item === 'middle') ? Math.floor(this.items.size() / 2) : o.item; //initial item
+			this.previous = this.current;
 			this.autoAdvanceInterval = null;
 
 			// Bind click events on individual items
@@ -51,14 +53,10 @@
 			});
 
 			// Center the actual parent's left side within it's parent
-			this.element.css(this.props[2],
-				(o.recenter ? -this.current * this.itemSize : 0)
-				+ (o.center ? this.element.parent()[0]['offset'+this.props[1]]/2 - this.itemSize : 0) //Center the items container
-				- (o.center ? parseInt(this.element.css('padding'+this.props[3]),10) || 0 : 0) //Subtract the padding of the items container
-			);
+			this._recenter();
 
 			// Jump to the first item
-			this._refresh(1, 0, this.current);
+			this._refresh(1, this.current, this.current);
 
 			if (o.autoAdvance > 0) {
 				if (o.pauseOnHover) {
@@ -71,6 +69,12 @@
 				}
 				// Initially start the auto advance timer
 				this.startAutoAdvance();
+			}
+
+			if (o.refreshOnWindowResize) {
+				$(window).resize(function() {
+					self._recenter();
+				});
 			}
 		},
 
@@ -95,62 +99,89 @@
 		},
 
 		select: function(item, noPropagation) {
+			var self = this, 
+				from = Math.abs(self.previous-self.current) <= 1 ? self.previous : self.current+(self.previous < self.current ? -1 : 1),
+				animation = {};
+
 			this.previous = this.current;
 			this.current = !isNaN(parseInt(item,10)) ? parseInt(item,10) : this.items.index(item);
 
-			// Don't animate when clicking on the same item
+			// Don't animate when selecting the same item
 			if (this.previous == this.current) {
 				return false
 			}
 
-			// Overwrite $.fx.step.coverflow everytime again with custom scoped values for this specific animation
-			var self = this, to = Math.abs(self.previous-self.current) <=1 ? self.previous : self.current+(self.previous < self.current ? -1 : 1);
-			$.fx.step.coverflow = function(fx) { 
-				self._refresh(fx.now, to, self.current); 
-			};
-			
 			// 1. Stop the previous animation
 			// 2. Animate the parent's left/top property so the current item is in the center
 			// 3. Use our custom coverflow animation which animates the item
-			var animation = { coverflow: 1 };
-
-			animation[this.props[2]] = (
-				(this.options.recenter ? -this.current * this.itemSize : 0)
-				+ (this.options.center ? this.element.parent()[0]['offset'+this.props[1]]/2 - this.itemSize : 0) // Center the items container
-				- (this.options.center ? parseInt(this.element.css('padding'+this.props[3]),10) || 0 : 0) // Subtract the padding of the items container
-			);
-
 			// Trigger the 'select' event/callback
 			if (!noPropagation) {
 				this._trigger('select', null, this._uiHash());
 			}
 
+			animation[this.props[2]] = this._getElementPosition();
 			this.element.stop().animate(animation, {
 				duration: this.options.duration,
-				easing: 'easeOutQuint'
+				easing: 'easeOutQuint',
+				step: function(now, fx) { 
+					self._refresh(fx.state, from, self.current); 
+				},
+				complete: function() {
+					self._adjustElements();
+					self._recenter();
+				}
 			});
 		},
 
-		_refresh: function(state,from,to) {
-			var self = this, offset = null;
-
+		_refresh: function(state, from, to) {
+			var self = this, 
+				offset = null, 
+				o = this.options, 
+				transferring = (from < to ? 0 : this.items.length - 1);
 			this.items.each(function(i) {
-				var side = (i == to && from-to < 0 ) ||  i-to > 0 ? 'left' : 'right',
-					mod = i == to ? (1-state) : ( i == from ? state : 1 ),
-					before = (i > from && i != to),
-					css = { zIndex: self.items.length + (side == "left" ? to-i : i-to) };
-					css[($.browser.safari ? 'webkit' : 'Moz')+'Transform'] = 'matrix(1,'+(mod * (side == 'right' ? -0.2 : 0.2))+',0,1,0,0) scale('+(1+((1-mod)*0.3)) + ')';
-					css[self.props[2]] = ((side === 'right' ? -1 : 1) * mod - i) * self.itemSize;
-
-				if(!supportsTransforms) {
-					css.width = self.itemWidth * (1+((1-mod)*0.5));
-					css.height = css.width * (self.itemHeight / self.itemWidth);
-					css.top = -((css.height - self.itemHeight) / 2);
+				var side = (((i == to) && (from < to)) || (i > to)) ? 'left' : 'right',
+					mod = (i === to) ? (1 - state) : ((i === from) ? state : 1),
+					css = { zIndex: self.items.length + (side == 'left' ? (to - i) : (i - to)) },
+					scale = (supportsTransforms && (i === transferring)) ? (state === 1 ? 1 : 1 - state) : (1+((1-mod)*o.currentItemZoom));
+				css[self.props[2]] = ((side === 'right' ? -1 : 1) * mod - i) * self.itemSize;
+				if (supportsTransforms) {
+					css[self.transformProp] = 'matrix(1,' + (mod * (side == 'right' ? -0.2 : 0.2)) + ',0,1,0,0) scale(' + scale + ')';
+				} else {
+					$.extend(css, {
+						width: self.itemWidth * scale,
+						height: self.itemHeight * scale,
+						top: -((self.itemHeight * (scale - 1)) / 2)
+					});
 				}
 				$(this).css(css);
 			});
-
 			this.element.parent().scrollTop(0);
+		},
+
+		_recenter: function() {
+			this.element.css(this.props[2], this._getElementPosition());
+		},
+
+		_getElementPosition: function() {
+			return (
+				(this.options.center ? this.element.parent()[0]['offset'+this.props[1]]/2 - this.itemSize : 0) // Center the items container
+				- (this.options.center ? parseInt(this.element.css('padding'+this.props[3]),10) || 0 : 0) // Subtract the padding of the items container
+				- (this.options.recenter ? this.current * this.itemSize : 0)
+			);
+		},
+
+		_adjustElements: function() {
+			var midpoint = this.items.length / 2;
+			while (this.current > midpoint) {
+				$(this.element).append(this.items[0]);
+				this.current += (this.current < midpoint) ? 1 : -1; // Adjust the current position because the array has wrapped
+			}
+			while (this.current < midpoint - 1) {
+				$(this.element).prepend(this.items[this.items.length - 1]);
+				this.current += (this.current < midpoint) ? 1 : -1; // Adjust the current position because the array has wrapped
+			}
+			this.items = $(this.options.items, this.element);
+			this._refresh(1, this.current, this.current);
 		},
 
 		_uiHash: function() {
